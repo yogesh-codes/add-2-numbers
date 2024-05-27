8404197768df339d8a58e9cc8355c21949b81f5b
Yogesh <139562574+yogesh-codes@users.noreply.github.com>
2024-05-27 20:06:57 +0530

    Init commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function add(){
+    let n1=parseInt(document.getElementById("num1").value);
+    let n2=parseInt(document.getElementById("num2").value);
+    let a=document.getElementById("answer-div");
+    let sum=n1+n2;
+    alert("Came here. sum  is"+n1+'...'+n2+"..."+a);
+    a.innerHTML=sum;
+}
